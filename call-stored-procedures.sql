0afd35262cc1acddda903765d76322d52812bb71
Kristine Anne C. Tabiolo <145534873+KristineTabs@users.noreply.github.com>
2024-07-11 22:04:20 +0800

    Create call-stored-procedures.sql
    This contains the call for the stored procedures from dml-kera

---
+++ call-stored-procedures.sql
@@ -0,0 +1,35 @@
+-- Calls the procedure Delete_Applicant(IN app_id_delete INT) to delete records of the applicant by using the app_id
+CALL Delete_Applicant(app_id_delete);
+
+-- Calls the procedure  Create_Applicant(new_app_name, ...) to create records of the applicant
+CALL Create_Applicant(
+	new_app_name,
+  new_date_birth,
+  new_app_add,
+  new_city,
+  new_state,
+  new_zip_code,
+  new_phone_num,
+  new_email_add,
+  new_emp_type,
+  new_pos_app,
+  new_des_sal,
+  new_date_start,
+  new_comp_name,
+  new_work_period,
+  new_comp_pos,
+  new_school_name,
+  new_school_loc,
+  new_date_grad,
+  new_educ_attain,
+  new_skill_one,
+  new_skill_two,
+  new_skill_three,
+  new_skill_four
+);
+
+-- Calls the procedure Display_Applicant() to view id, name, address, and email address of the applicant and the application status
+CALL Display_Applicant();
+
+-- Calls the procedure Update_Status(update_application) to update the status of the application
+CALL Update_Status(update_application);
