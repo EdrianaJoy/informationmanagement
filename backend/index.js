15435f4202e68aa3c3e0129d54d2690ed643d86e
Geru <113689254+Gelatino0308@users.noreply.github.com>
2024-07-13 04:19:09 +0800

    fixed create

--- backend/index.js
+++ backend/index.js
@@ -42,13 +42,36 @@ app.post('/api/createapp', (req, res) => {
           state, 
           zip_code, 
           phone_num, 
-          email_add } = req.body;
+          email_add, 
+          emp_type, 
+          pos_app, 
+          des_sal, 
+          jobstart,
+          comp_name_1, 
+          doy_1, 
+          pos_comp_1, 
+          comp_name_2, 
+          doy_2, 
+          pos_comp_2, 
+          comp_name_3, 
+          doy_3, 
+          pos_comp_3, 
+          school_name, 
+          school_loc, 
+          date_grad, 
+          educ_attain, 
+          skill_one, 
+          skill_two, 
+          skill_three, 
+          skill_four } = req.body;
 
   console.log(app_name, date_birth, app_add, city, state, zip_code, phone_num, email_add);
 
-  dbConnection.query('CALL Create_Applicant(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
+  dbConnection.query('CALL Create_Applicant(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
               ,[app_name, date_birth, app_add, city, state, zip_code, phone_num, email_add, 
-                emp_type, pos_app, des_sal, jobstart, school_name, school_loc, date_grad, educ_attain, 
+                emp_type, pos_app, des_sal, jobstart, comp_name_1, doy_1, pos_comp_1, comp_name_2, doy_2, pos_comp_2, 
+                comp_name_3, doy_3, pos_comp_3, school_name, school_loc, date_grad, 
+                educ_attain, skill_one, skill_two, skill_three, skill_four
               ], (err, result) => {
     if (err) {
       console.error('Error executing query: ' + err.stack);
@@ -72,10 +95,10 @@ app.get('/api/displayapp', (req, res) => {
 });
 
 
-app.put('/api/applicants/:id', (req, res) => {
-  const { app_name } = req.body;
+app.put('/api/updateapp/:id', (req, res) => {
+  const { app_status } = req.body;
   const appId = req.params.id;
-  dbConnection.query('UPDATE applicant SET app_name = ? WHERE app_id = ?;', [app_name, appId], (err, result) => {
+  dbConnection.query('CALL Update_Status(?, ?);', [app_status, appId], (err, result) => {
     if (err) {
       console.error('Error executing query: ' + err.stack);
       res.status(400).send('Error updating request status');
@@ -86,9 +109,9 @@ app.put('/api/applicants/:id', (req, res) => {
 });
 
 
-app.delete('/api/applicants/:id', (req, res) => {
+app.delete('/api/deleteapp/:id', (req, res) => {
     const appId = req.params.id;
-    dbConnection.query('DELETE FROM applicant WHERE app_id = ?;', [appId], (err, result) => {
+    dbConnection.query('CALL Delete_Applicant(?);', [appId], (err, result) => {
       if (err) {
         console.error('Error executing query: ' + err.stack);
         res.status(400).send('Error deleting applicants');
