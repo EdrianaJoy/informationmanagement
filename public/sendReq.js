2527e49d07ad7da26b54b2c9affb0acfe8189818
RJ <ricajoicmigueh@iskolarngbayan.pup.edu.ph>
2024-07-12 23:04:56 +0800

    updated job.html for manual session data storage

--- public/sendReq.js
+++ public/sendReq.js
@@ -1,27 +1,27 @@
 function submitForm() {
-     const formData = {};
- 
-     const pageData = JSON.parse(sessionStorage.getItem(`formPage${1}`)); //should be a loop that iterates through each form per page
-     Object.assign(formData, pageData);
-     
+   const formData = {};
 
-     // Send data via Fetch API
-     fetch('/api/createapp', {
-     method: 'POST',
-     headers: {
+   const pageData = JSON.parse(sessionStorage.getItem(`formPage${1}`)); //should be a loop that iterates through each form per page
+   Object.assign(formData, pageData);
+
+
+   // Send data via Fetch API
+   fetch('/api/createapp', {
+      method: 'POST',
+      headers: {
          'Content-Type': 'application/json'
-     },
-     body: JSON.stringify(formData)
-     })
-     .then(response => response.json())
-     .then(data => {
-     console.log('Success:', data);
-     // Clear sessionStorage if needed
-     sessionStorage.clear();
-     })
-     .catch((error) => {
-     console.error('Error:', error);
-     });
- }
+      },
+      body: JSON.stringify(formData)
+   })
+      .then(response => response.json())
+      .then(data => {
+         console.log('Success:', data);
+         // Clear sessionStorage if needed
+         sessionStorage.clear();
+      })
+      .catch((error) => {
+         console.error('Error:', error);
+      });
+}
 
- document.getElementById('submit-btn').addEventListener('click', submitForm);
+document.getElementById('submit-btn').addEventListener('click', submitForm);
